b93ff48c2d09bb56fecb11282ed01bc1ec3d8371
Bennik2000 <bennik.ko@gmail.com>
2020-03-25 22:05:39 +0100

    Add copy to clipboard button

---
+++ LaTeXTableGenerator/UI/Commands/CopyToClipboardCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LaTeXTableGenerator.UI.Commands
+{
+    public class CopyToClipboardCommand : ICommand
+    {
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            if (parameter is string str)
+            {
+                System.Windows.Clipboard.SetText(str);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
