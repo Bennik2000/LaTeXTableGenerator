2b552fd65a71aaf3ce6ce23d84db218f3ee23e71
Bennik2000 <bennik.ko@gmail.com>
2020-03-25 21:31:31 +0100

    Add ribbon

--- LaTeXTableGenerator/MainWindow.xaml.cs
+++ LaTeXTableGenerator/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace LaTeXTableGenerator
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
